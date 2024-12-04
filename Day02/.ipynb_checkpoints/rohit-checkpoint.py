6c02b7ef3e197023bf2b35e000884c89b995b454
ImranAhsann <125964705+ImranAhsann@users.noreply.github.com>
2024-12-04 20:31:57 +0500

    python files upload

---
+++ Day02/.ipynb_checkpoints/rohit-checkpoint.py
@@ -0,0 +1,8 @@
+name= input(("Enter your name "))
+
+if name == "hassan":
+    print ("You are Rohit")
+
+else: 
+    print("You are not Rohit")
+
---
+++ Day02/test.py
@@ -0,0 +1,10 @@
+while True:
+    # Prompt the user to enter their name
+    name = input("Enter your name: ")
+
+    # Check if the entered name is "hassan"
+    if name == "hassan" or name =="hasan":
+        print("You are Rohit")
+        break  # Exit the loop if the name is "hassan"
+    else:
+        print("You are not Rohit")
